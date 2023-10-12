6decf5cdd38062c5d0c44275bc6ac7dbd61fd803
TheSang88 <phamthesang88@gmail.com>
2023-10-12 14:56:18 +0700

    clienteco

---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import Signup from "../components/Signup/Signup";
+
+const SignupPage = () => {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (isAuthenticated === true) {
+      navigate("/");
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  return (
+    <div>
+      <Signup />
+    </div>
+  )
+}
+
+export default SignupPage
