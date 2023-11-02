992c3e10ef89c9c72e332aa38fd2ce2cda91fb3d
TheSang88 <128338833+TheSang88@users.noreply.github.com>
2023-11-02 15:45:50 +0700

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Header from "../components/Layout/Header";
 import Hero from "../components/Route/Hero/Hero";
 import Categories from "../components/Route/Categories/Categories";
@@ -7,18 +7,38 @@ import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct
 import Events from "../components/Events/Events";
 import Sponsored from "../components/Route/Sponsored";
 import Footer from "../components/Layout/Footer";
-
+import Loading from './loading';
+import axios from 'axios';
 const HomePage = () => {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    axios.get('https://servereco.onrender.com')
+        .then(result => {
+            setLoading(false);
+        })
+        .catch(err => {
+            setLoading(true);
+            console.log(err)
+        })
+}, [])
+
   return (
+
     <div>
-        <Header activeHeading={1} />
-        <Hero />
-        <Categories />
-        <BestDeals />
-        <Events />
-        <FeaturedProduct />
-        <Sponsored />
-        <Footer />
+      {loading ? (<Loading />) : (
+          <div>
+            <Header activeHeading={1} />
+            <Hero />
+              <Categories />
+            <BestDeals />
+            <Events />
+            <FeaturedProduct />
+            <Sponsored />
+             <Footer />
+            </div>
+
+  )}
     </div>
   )
 }
